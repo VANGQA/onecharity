87bcda50d5a851e6bf56dcf1ea5012983250130a
VANGQA <140230286+VANGQA@users.noreply.github.com>
2023-08-03 05:23:06 -0700

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -10,6 +10,7 @@ import { listTopSellers } from '../actions/userActions';
 import { Link } from 'react-router-dom';
 import Slider from '../components/Slider';
 import slides from '../mock.json';
+import { MDBContainer, MDBRow, MDBCol, MDBRipple } from "mdb-react-ui-kit";
 
 export default function HomeScreen() {
   const dispatch = useDispatch();
@@ -23,9 +24,17 @@ export default function HomeScreen() {
     users: sellers,
   } = userTopSellersList;
 
+const productCategoryList = useSelector((state) => state.productCategoryList);
+  const {
+    loading: loadingCategories,
+    error: errorCategories,
+    categories,
+  } = productCategoryList;
+
   useEffect(() => {
     dispatch(listProducts({}));
     dispatch(listTopSellers());
+    dispatch(listProductCategories());
   }, [dispatch]);
   return (
     <div>
@@ -52,6 +61,46 @@ export default function HomeScreen() {
           </Carousel>
         </>
       )}
+        <MDBContainer fluid className="my-5 text-center">
+        <h4 className="mt-4 mb-5">
+            <strong>Product Listing</strong>
+        </h4>
+        <MDBRow>
+        <MDBCol md="12" lg="4" className="mb-4">
+          <MDBRipple
+            rippleColor="dark"
+            rippleTag="div"
+            className="bg-image rounded hover-zoom shadow-1-strong"
+          >
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/new/img(1).webp"
+              fluid
+              className="w-100"
+            />
+            <a href="#!">
+              <div
+                className="mask"
+                style={{ backgroundColor: "rgba(0, 0, 0, 0.3)" }}
+              >
+                <div className="d-flex justify-content-start align-items-start h-100">
+                  <h5>
+                    <span className="badge bg-light pt-2 ms-3 mt-3 text-dark">
+                      $123
+                    </span>
+                  </h5>
+                </div>
+              </div>
+              <div className="hover-overlay">
+                <div
+                  className="mask"
+                  style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
+                ></div>
+              </div>
+            </a>
+          </MDBRipple>
+        </MDBCol>
+        </MDBRow>
+        </MDBContainer>
       <h2>Featured Products</h2>
       {loading ? (
         <LoadingBox></LoadingBox>
