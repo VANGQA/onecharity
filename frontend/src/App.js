deccef0568d0edd97d25aeb9e02229d0a91caad9
VANGQA <140230286+VANGQA@users.noreply.github.com>
2023-08-04 07:10:53 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -324,7 +324,7 @@ function App() {
           </Routes>
         </main>
        <MDBFooter className='text-center' color='white' bgColor='dark'>
-      <MDBContainer className='p-4'>
+        <MDBContainer className='p-4'>
         <section className='mb-4'>
           <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
             <MDBIcon fab icon='facebook-f' />
