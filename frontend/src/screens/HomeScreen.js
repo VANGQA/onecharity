a1cbb6981d198fcc72bfb80ed2cdc4cf7b4fbd8a
VANGQA <140230286+VANGQA@users.noreply.github.com>
2023-08-07 06:54:52 -0700

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -8,6 +8,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { listProducts,} from '../actions/productActions';
 import { listTopSellers } from '../actions/userActions';
 import { Link } from 'react-router-dom';
+import Slider from './slider';
+import slides from './mock.json'
 
 import { MDBContainer, MDBRow, MDBCol, MDBRipple } from "mdb-react-ui-kit";
 
