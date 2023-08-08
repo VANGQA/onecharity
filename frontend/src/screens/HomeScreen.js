a7d74ab0010a83ae68cd9ae7e563d33c737e9cd9
VANGQA <140230286+VANGQA@users.noreply.github.com>
2023-08-08 05:15:47 -0700

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,13 +5,9 @@ import Product from '../components/Product';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { useDispatch, useSelector } from 'react-redux';
-import { listProducts,} from '../actions/productActions';
+import { listProducts } from '../actions/productActions';
 import { listTopSellers } from '../actions/userActions';
 import { Link } from 'react-router-dom';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-
-
-import { MDBContainer, MDBRow, MDBCol, MDBRipple } from "mdb-react-ui-kit";
 
 export default function HomeScreen() {
   const dispatch = useDispatch();
@@ -25,16 +21,12 @@ export default function HomeScreen() {
     users: sellers,
   } = userTopSellersList;
 
-
-
   useEffect(() => {
     dispatch(listProducts({}));
     dispatch(listTopSellers());
-    
   }, [dispatch]);
   return (
-
-  <div>
+    <div>
       <h2>Top Sellers</h2>
       {loadingSellers ? (
         <LoadingBox></LoadingBox>
@@ -51,49 +43,10 @@ export default function HomeScreen() {
                   <p className="legend">{seller.seller.name}</p>
                 </Link>
               </div>
-           </Carousel>
+            ))}
+          </Carousel>
         </>
       )}
-        <MDBContainer fluid className="my-5 text-center">
-        <h4 className="mt-4 mb-5">
-            <strong>Product Listing</strong>
-        </h4>
-        <MDBRow>
-        <MDBCol md="12" lg="4" className="mb-4">
-          <MDBRipple
-            rippleColor="dark"
-            rippleTag="divv"
-            className="bg-image rounded hover-zoom shadow-1-strong"
-          >
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/new/img(1).webp"
-              fluid
-              className="w-100"
-            alt="category-img" />
-            <a href="#!">
-              <div
-                className="mask"
-                style={{ backgroundColor: "rgba(0, 0, 0, 0.3)" }}
-              >
-                <div className="d-flex justify-content-start align-items-start h-100">
-                  <h5>
-                    <span className="badge bg-light pt-2 ms-3 mt-3 text-dark">
-                      $123
-                    </span>
-                  </h5>
-                </div>
-              </div>
-              <div className="hover-overlay">
-                <div
-                  className="mask"
-                  style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-                ></div>
-              </div>
-            </a>
-          </MDBRipple>
-        </MDBCol>
-        </MDBRow>
-        </MDBContainer>
       <h2>Featured Products</h2>
       {loading ? (
         <LoadingBox></LoadingBox>
