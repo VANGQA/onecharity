31a24679dfd48ae864ad0899a5552bd7ffc7d946
VANGQA <140230286+VANGQA@users.noreply.github.com>
2023-08-08 04:54:09 -0700

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -35,11 +35,13 @@ export default function HomeScreen() {
   return (
 
     <Carousel showThumbs={false} autoPlay>
+         (
           <div>
               <a className="flex">
                 <img src= alt="https://media.takealot.com/promotions/83540-top-banner.png" />
               </a>
-          </div>  
+          </div>
+        )
     </Carousel>
     
   <div>
