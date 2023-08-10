f4eb6464d22287584ce12a98c585ce8b4f04a10a
VANGQA <140230286+VANGQA@users.noreply.github.com>
2023-08-10 02:27:48 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,8 +31,6 @@ import MapScreen from './screens/MapScreen';
 import DashboardScreen from './screens/DashboardScreen';
 import SupportScreen from './screens/SupportScreen';
 import ChatBox from './components/ChatBox';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
-import { Carousel } from 'react-responsive-carousel';
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -173,22 +171,7 @@ function App() {
             )}
           </ul>
         </aside>
-             <>
-                <Carousel>
-                    <div>
-                        <img src="https://tpc.googlesyndication.com/simgad/436127923589322544?" height="100px" width="200px" />
-                        <p className="legend">Legend 1</p>
-                    </div>
-                    <div>
-                        <img src="https://tpc.googlesyndication.com/simgad/6851203265671556033?" height="100px" width="200px" />
-                        <p className="legend">Legend 2</p>
-                    </div>
-                    <div>
-                        <img src="https://www.takealot.com/deals/83540" height="100px" width="200px" />
-                        <p className="legend">Legend 3</p>
-                    </div>
-                </Carousel>
-              </>
+             
         <main>
           <Routes>
             <Route path="/seller/:id" element={<SellerScreen />}></Route>
