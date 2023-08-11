28086ffb347ee4d13aee379f84d8b7f2494376ae
VANGQA <140230286+VANGQA@users.noreply.github.com>
2023-08-11 01:22:03 +0100

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,55 +1,69 @@
-import React, { useEffect } from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import Product from '../components/Product';
-import LoadingBox from '../components/LoadingBox';
-import MessageBox from '../components/MessageBox';
-import { useDispatch, useSelector } from 'react-redux';
-import { listProducts } from '../actions/productActions';
+import { useEffect, useReducer, useState } from "react";
+import axios from "axios";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Product from "../components/Product";
+import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+// import data from '../data';
 
-import { Link } from 'react-router-dom';
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
-export default function HomeScreen() {
-  const dispatch = useDispatch();
-  const productList = useSelector((state) => state.productList);
-  const { loading, error, products } = productList;
-
- 
-
+function HomeScreen() {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    products: [],
+    loading: true,
+    error: "",
+  });
+  // const [products, setProducts] = useState([]);
   useEffect(() => {
-    dispatch(listProducts({}));
-   
-  }, [dispatch]);
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+
+      // setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
-     
     <div>
-        <>
-            <Carousel>
-                <div>
-                    <img src="https://m.media-amazon.com/images/I/51RPiINhM7L._SX1500_.jpg"  />
-                    <p >Legend 1</p>
-                </div>
-                <div>
-                    <img src="https://m.media-amazon.com/images/I/51RPiINhM7L._SX1500_.jpg"  />
-                    <p >Legend 1</p>
-                </div>
-           
-            </Carousel>
-         </>
-      <h2>Featured Products</h2>
-      {loading ? (
-        <LoadingBox></LoadingBox>
-      ) : error ? (
-        <MessageBox variant="danger">{error}</MessageBox>
-      ) : (
-        <>
-          {products.length === 0 && <MessageBox>No Product Found</MessageBox>}
-          <div className="row center">
+      <Helmet>
+        <title>Amazona</title>
+      </Helmet>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {loading ? (
+          <LoadingBox />
+        ) : error ? (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+          <Row>
             {products.map((product) => (
-              <Product key={product._id} product={product}></Product>
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
             ))}
-          </div>
-        </>
-      )}
+          </Row>
+        )}
+      </div>
     </div>
   );
 }
+export default HomeScreen;
