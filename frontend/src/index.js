1716dc3aea9f2ba3b51074c97227da698bb2fcfd
VANGQA <140230286+VANGQA@users.noreply.github.com>
2023-08-11 01:20:48 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 import store from './store';
 
 root.render(
@@ -23,4 +23,4 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+reportWebVitals();
