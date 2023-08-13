176d3e25b183445dc98e33e09d9bc60cbc91cf48
VANGQA <140230286+VANGQA@users.noreply.github.com>
2023-08-13 03:57:51 +0100

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -11,6 +11,13 @@ import HeroSlider, {Slide} from 'hero-slider';
 import Carousel from 'react-bootstrap/Carousel';
 import ExampleCarouselImage from 'components/ExampleCarouselImage';
 
+function ControlledCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
 export default function HomeScreen() {
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
@@ -31,21 +38,21 @@ export default function HomeScreen() {
     <div>
       <Carousel>
       <Carousel.Item>
-        <ExampleCarouselImage text="First slide" />
+       
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Second slide" />
+        
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Third slide" />
+        
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
