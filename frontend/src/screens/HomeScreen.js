4427bc1b4dceba85592c8ca74e0381f79a9267f0
VANGQA <140230286+VANGQA@users.noreply.github.com>
2023-08-13 02:59:44 +0100

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,6 +9,7 @@ import { listTopSellers } from '../actions/userActions';
 import { Link } from 'react-router-dom';
 import HeroSlider, {Slide} from 'hero-slider';
 import Carousel from 'react-bootstrap/Carousel';
+import ExampleCarouselImage from 'components/ExampleCarouselImage';
 
 export default function HomeScreen() {
   const dispatch = useDispatch();
