a3a6a5c82636bc6f000f5b5310f10602ff376d4d
VANGQA <140230286+VANGQA@users.noreply.github.com>
2023-08-13 02:38:40 +0100

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,11 +9,7 @@ import { listProducts } from '../actions/productActions';
 import { listTopSellers } from '../actions/userActions';
 import { Link } from 'react-router-dom';
 import HeroSlider, {Slide} from 'hero-slider';
-
-const takeaBanner="https://media.takealot.com/promotions/83540-top-banner.png";
-const takedProduct="https://media.takealot.com/promotions/app-only-top-banner.png";
-const afterProfuct="https://media.takealot.com/promotions/82109-top-banner.png";
-const theProfuct="https://media.takealot.com/promotions/82109-top-banner.png";
+import Carousel from 'react-bootstrap/Carousel';
 
 export default function HomeScreen() {
   const dispatch = useDispatch();
@@ -34,14 +30,30 @@ export default function HomeScreen() {
   return (
     <div>
       <Carousel>
-                <div>
-                    <img src="https://media.takealot.com/promotions/83540-top-banner.png" />
-                    <img src="https://media.takealot.com/promotions/83540-top-banner.png" />
-                    <img src="https://media.takealot.com/promotions/83540-top-banner.png" />
-               
-                </div>
-                
-            </Carousel>
+      <Carousel.Item>
+        <ExampleCarouselImage text="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <ExampleCarouselImage text="Second slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <ExampleCarouselImage text="Third slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
    
       <h2>Featured Products</h2>
       {loading ? (
