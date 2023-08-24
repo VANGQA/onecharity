edda012f9b8e38ef50109d223fb410c4b8badcad
VANGQA <codemydotcom@gmail.com>
2023-08-24 14:39:14 -0700

    Home-commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -67,7 +67,7 @@ function App() {
               <i className="fa fa-bars"></i>
             </button>
             <Link className="brand" to="/">
-              amazona
+            Curiosity Charity
             </Link>
           </div>
           <div>
